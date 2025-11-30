e91f4cf73c9dba2e1b8fa29c01f30edaea7c4110
sofiadanilik <sofiadanilik@gmail.com>
2025-11-30 17:40:06 +0200

    Update Program.cs

--- Ssss/Program.cs
+++ Ssss/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, this is updated version from Sofiia on GitHub!");
         }
     }
 }
